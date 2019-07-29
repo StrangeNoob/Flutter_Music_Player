167808fe8f0d480acf4e3cdc7e43fa9dc5b0efbf
StrangeNoob <sipun63427452@gmail.com>
2019-07-29 23:18:11 +0530

    "Initial Release"

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:musix/theme.dart';
+import 'package:musix/my_app.dart';
+void main() => runApp(new MyMaterialApp());
+
+class MyMaterialApp extends StatefulWidget {
+  @override
+  MyMaterialAppState createState() {
+    return new MyMaterialAppState();
+  }
+}
+
+class MyMaterialAppState extends State<MyMaterialApp> {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        debugShowCheckedModeBanner: false, theme: darkTheme, 
+        home: new MyApp(),
+        );
+  }
+}
